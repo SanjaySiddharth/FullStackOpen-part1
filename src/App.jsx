fd146f3b8c4411934e3fb1edf72cd7fb44a6da2f
SanjaySiddharth <mjsanjaysiddharth1999@gmail.com>
2024-05-21 13:17:39 +0530

    Handling Arrays

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
-  const increaseByOne = () => setCounter(counter+1)
-  const reset =()=> setCounter(0)
-  const decreaseByOne = () => setCounter(counter-1)
+  
+  const [left,setLeft] = useState(0)
+  const [right,setRight] = useState(0)
+  const [allClicks,setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left+1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right+1)
+  }
+
   return (
     <>
-    <Display counter={counter}></Display>
-    <Button onClick={increaseByOne} text='plus'></Button>
-    <Button onClick={reset} text='reset'></Button>
-    <Button onClick={decreaseByOne} text='minus'></Button>
+      {left}
+      <button onClick={handleLeftClick}>Left</button>
+      <button onClick={handleRightClick}>Right</button>
+      {right}
+      <p>
+        {allClicks.join(' ')}
+      </p>
     </>
   )
 }
@@ -18,17 +32,3 @@ const App = () => {
 
 export default App
 
-const Display = (props) => {
-  return (
-    <div> {props.counter}
-    </div>
-  )
-}
-
-const Button = (props) => {
-  return (
-    <button onClick={props.onClick}>
-        {props.text}
-    </button>
-  )
-}
